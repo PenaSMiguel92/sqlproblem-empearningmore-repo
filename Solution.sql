ad03e819b2d1e981b5261e8e1767347f99a10848
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-29 15:03:41 -0700

    final solution to sql problem

--- Solution.sql
+++ Solution.sql
@@ -1,2 +1,3 @@
-SELECT emp.name AS Employee FROM employee AS emp1 
-INNER JOIN employee AS emp2 ON emp1.id = emp2.managerId
+SELECT emp1.name AS Employee FROM employee AS emp1 
+INNER JOIN employee AS emp2 ON emp1.managerId = emp2.id
+WHERE emp1.salary > emp2.salary AND emp1.managerId = emp2.id
